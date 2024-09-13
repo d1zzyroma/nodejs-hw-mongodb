52f8682c8f5ba291140e00398bd0e6ad58a3bcf9
d1zzyroma <nataliawologin@gmail.com>
2024-09-13 14:04:40 +0200

    321

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+
 import setupServer from "./server.js";
 import { initMongoDB } from "./db/initMongoConnection.js";
 
+
 const bootstrap = async () => {
-    try {
-        await initMongoDB();
-        setupServer();
-    } catch (error) {
-        console.error("Failed to initialize the application:", error);
-        process.exit(1); // Exit the process with a failure code
-    }
+  await initMongoDB();
+  setupServer();
 };
 
 bootstrap();
